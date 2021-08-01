a24c18b604fc32a14e804bda5032b3a00c1cd260
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-01 23:19:08 +0600

    for loop and print odd and even numbers with for loop

---
+++ for-loop.js
@@ -0,0 +1,19 @@
+for (var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('roast den vai:');
+    console.log(roastGiven);
+}
+
+for ( var i = 1; i < 7; i++){
+    console.log('roast diyen na ar');
+    console.log(i);
+}
+
+// odd numbers 
+for ( var i = 1; i <= 20; i += 2){
+    console.log(i);
+}
+
+// even numbers 
+for (var i = 2; i <= 20; i += 2){
+    console.log(i);
+}
