bdf2c8ce0b94f8d906dcf107245237c6d83c6d59
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-01 23:01:49 +0600

    printing odd and even numbers using while loop

---
+++ while-loop-example.js
@@ -0,0 +1,19 @@
+var number = 1;
+while (number <= 10) {
+    console.log(number);
+    number++;
+}
+
+// print odd numbers
+number = 1; 
+while (number <= 20) {
+    console.log(number);
+    number += 2;
+}
+
+// print even numbers
+number = 2; 
+while (number <= 20) {
+    console.log(number);
+    number += 2;
+}
