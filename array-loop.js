f456debdae935de28e2f8eee4558e988d9312d88
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-01 23:50:57 +0600

    learning for loop with a different aangle

---
+++ array-loop.js
@@ -0,0 +1,13 @@
+var numbers = [4, 64, 23, 94, 83, 44, 45, 50, 51];
+
+for(var i = 0; i < numbers.length; i++){
+    var number = numbers[i];
+    console.log(number);
+}
+
+var names = ['Jorina', 'Tina', 'Rohima', 'Korima', 'Borsha', 'Shondha', 'Monisha', 'Ratri'];
+for (let i = 0; i < names.length -1; i++) {
+    var element = names[i];
+    console.log(element);
+    
+}
